80c00d351181a0903eda9bfb6b216bda9f063ff9
Nourhan Ahmed <46425742+nourhanahmedelbadawie@users.noreply.github.com>
2026-02-09 16:19:30 +0200

    _

---
+++ 0x04-TypeScript/task_1/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  entry: {
+    all: ["./js/main.ts"],
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./dist/index.html",
+    }),
+  ],
+};
